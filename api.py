57034eebad40471e5f3ac9e7f548628b740c08a0
DreamWalkerXZ <dreamwalkerxz@outlook.com>
2022-04-03 00:03:32 +0800

    v0.1

--- api.py
+++ api.py
@@ -68,13 +68,14 @@ class TrojanServer(object):
             print(i)
 
 
-def generateUser(password, hash='', upload_speed_limit=39321600, download_speed_limit=39321600, ip_limit=4):
+def generateUser(password, hash='', upload_traffic=0, download_traffic=0, upload_speed_limit=39321600, download_speed_limit=39321600, ip_limit=4):
     user = api_pb2.UserStatus(
         user=api_pb2.User(
             password=password,
             hash=hash
         ),
-        traffic_total=api_pb2.Traffic(upload_traffic=0, download_traffic=0),
+        traffic_total=api_pb2.Traffic(
+            upload_traffic=upload_traffic, download_traffic=download_traffic),
         speed_current=api_pb2.Speed(upload_speed=0, download_speed=0),
         speed_limit=api_pb2.Speed(
             upload_speed=upload_speed_limit, download_speed=download_speed_limit),
