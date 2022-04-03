5602b1cdde85a07529121c2ea24766b7daf4b4b2
DreamWalkerXZ <dreamwalkerxz@outlook.com>
2022-04-04 00:39:09 +0800

    v0.2

--- api.py
+++ api.py
@@ -3,6 +3,10 @@ import grpc
 import api_pb2
 import api_pb2_grpc
 
+from rich.console import Console
+
+console = Console()
+
 
 class TrojanServer(object):
     def __init__(self, server, port) -> None:
@@ -23,52 +27,32 @@ class TrojanServer(object):
     def add_users(self, users):
         reqs = []
         for user in users:
-            if hasattr(user.user, 'password'):
-                print(f'Adding user({user.user.password})')
-            elif hasattr(user.user, 'hash'):
-                print(f'Adding user({user.user.hash})')
-            else:
-                print(f'Adding user({user.user.password} - {user.user.hash})')
             reqs.append(api_pb2.SetUsersRequest(status=user,
                         operation=api_pb2.SetUsersRequest.Operation.Add))
         res = self.stub.SetUsers(iter(reqs))
         for i in res:
-            print(i)
+            console.log(i)
 
     def delete_users(self, users):
         reqs = []
         for user in users:
-            if hasattr(user.user, 'password'):
-                print(f'Deleting user({user.user.password})')
-            elif hasattr(user.user, 'hash'):
-                print(f'Deleting user({user.user.hash})')
-            else:
-                print(
-                    f'Deleting user({user.user.password} - {user.user.hash})')
             reqs.append(api_pb2.SetUsersRequest(status=user,
                         operation=api_pb2.SetUsersRequest.Operation.Delete))
         res = self.stub.SetUsers(iter(reqs))
         for i in res:
-            print(i)
+            console.log(i)
 
     def modify_users(self, users):
         reqs = []
         for user in users:
-            if hasattr(user.user, 'password'):
-                print(f'Modifying user({user.user.password})')
-            elif hasattr(user.user, 'hash'):
-                print(f'Modifying user({user.user.hash})')
-            else:
-                print(
-                    f'Modifying user({user.user.password} - {user.user.hash})')
             reqs.append(api_pb2.SetUsersRequest(status=user,
                         operation=api_pb2.SetUsersRequest.Operation.Modify))
         res = self.stub.SetUsers(iter(reqs))
         for i in res:
-            print(i)
+            console.log(i)
 
 
-def generate_user(password, hash='', upload_traffic=0, download_traffic=0, upload_speed_limit=39321600, download_speed_limit=39321600, ip_limit=4):
+def generate_user(password, hash='', upload_traffic=0, download_traffic=0, upload_speed_limit=39321600, download_speed_limit=39321600, upload_speed_current=0, download_speed_current=0, ip_limit=4):
     user = api_pb2.UserStatus(
         user=api_pb2.User(
             password=password,
@@ -76,7 +60,8 @@ def generate_user(password, hash='', upload_traffic=0, download_traffic=0, uploa
         ),
         traffic_total=api_pb2.Traffic(
             upload_traffic=upload_traffic, download_traffic=download_traffic),
-        speed_current=api_pb2.Speed(upload_speed=0, download_speed=0),
+        speed_current=api_pb2.Speed(
+            upload_speed=upload_speed_current, download_speed=download_speed_current),
         speed_limit=api_pb2.Speed(
             upload_speed=upload_speed_limit, download_speed=download_speed_limit),
         ip_current=0,
